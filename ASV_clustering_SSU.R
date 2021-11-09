69e48690b1e04603b71d8449712f80cc8c88ed80
jhyang0113 <49726623+jhyang0113@users.noreply.github.com>
2021-11-09 16:17:40 -0600

    Update ASV_clustering_SSU.R

--- ASV_clustering_SSU.R
+++ ASV_clustering_SSU.R
@@ -8,20 +8,18 @@ path <- "/mnt/scratch/f0008425/SSU/filtered"
 set.seed(1390)
 CORES = parallel::detectCores()
 
+# Load dataset
 seqtab <- readRDS(file='seq_table.RDS')
+taxtab <- readRDS(file='tax_table.RDS')
 seqtab <- t(seqtab)
 asv_sequences <- colnames(seqtab)
 sample_names <- rownames(seqtab)
 dna <- Biostrings::DNAStringSet(asv_sequences)
 
-## Find clusters of ASVs to form the new OTUs
-aln <- DECIPHER::AlignSeqs(dna, processors = CORES)
-d <- DECIPHER::DistanceMatrix(aln, processors = CORES)
-clusters <- DECIPHER::IdClusters(
-  d, 
-  method = "complete",
-  cutoff = 0.03, # use `cutoff = 0.03` for a 97% OTU 
-  processors = CORES)
+# ASV clustering (use `cutoff = 0.03` for a 97% OTU)
+alnseq <- DECIPHER::AlignSeqs(dna, processors = nprocessor)
+dm <- DECIPHER::DistanceMatrix(alnseq, processors = nprocessor)
+clusters <- DECIPHER::IdClusters(dm, method = "complete", cutoff = 0.03, processors = nprocessor)
 
 ## Use dplyr to merge the columns of the seqtab matrix for ASVs in the same OTU
 # prep by adding sequences to the `clusters` data frame
@@ -32,17 +30,24 @@ merged_seqtab <- seqtab %>%
   rowsum(clusters$cluster) %>%
   t
 
-# Representative sequences extracting
-newdf<-as.data.frame(cbind.data.frame(rownames(clusters),clusters$cluster,clusters$sequence))
-colnames(newdf)<-c("ASV","Number_of_ASV","sequence") 
-newdf.sort<-newdf %>% arrange(Number_of_ASV) %>% distinct(Number_of_ASV,.keep_all = TRUE)
-fas<-paste(">OTU",seq(1:nrow(newdf.sort)),"\t",newdf.sort$sequence,sep="")
-
 # Optional renaming of clusters to OTU<cluster #>
 colnames(merged_seqtab) <- paste0("OTU", colnames(merged_seqtab))
 
-write.csv(t(merged_seqtab), file="OTU_table.csv")
+# Representative sequences extracting (new OTU, representative ASV, its sequences)
+newdf<-as.data.frame(cbind.data.frame(clusters$cluster, rownames(clusters),clusters$sequence))
+colnames(newdf)<-c("OTU","ASV","sequence") 
+newdf.sort<-newdf %>% arrange(OTU) %>% distinct(OTU,.keep_all = TRUE)
+
+# Create tax table
+taxlist<-as.data.frame(paste("ASV",newdf.sort$ASV, sep=""))
+colnames(taxlist)<-c("ASV")
+entiretax <- as.data.frame(paste("ASV",seq(1:nrow(taxtab)), sep=""))
+colnames(entiretax)<-c("ASV")
+entiretax <- cbind(entiretax, as.data.frame(taxtab))
+tax_table_otu <- merge(taxlist, entiretax[,c("ASV", "Kingdom", "Phylum","Class","Order", "Family", "Genus", "Species")], by="ASV")
+                      
+# Create the final dataset for further analysis
+write.csv(t(merged_seqtab), file="otu_table_otu.csv")
+write.csv(tax_table_otu, file="tax_table_otu.csv", row.names = FALSE)
 write.csv(clusters, file="cluster_results.csv")
-write.csv(fas,file="representative_seqs.csv", row.names=FALSE, col.names=FALSE,quote = FALSE)
-
-
+write.csv(newdf.sort,file="representative_seqs.csv", row.names=FALSE, col.names=FALSE,quote = FALSE)
